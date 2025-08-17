05628d454fb0855ac2e6f289cbeb3a50ee05e709
Allan Mathenge <allanmathenge67@gmail.com>
2025-08-17 12:25:00 +0300

    added .env and .env.* in the .gitignore file as well as added the environment files for development, sit, uat, and production ... also updated the root package.json file to cater for these enviroments

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //Make config available accross all modules
+      envFilePath: [`.env.${process.env.NODE || 'development'}`] // Loads .env.development or .env.production depending on NODE_ENV
+    })
+  ],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,18 +6,28 @@
 import { Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api-gateway';
-  app.setGlobalPrefix(globalPrefix);
 
+  //config service 
+  const configService = app.get(ConfigService)
+
+  //Setting up the global prefix via the config service
+  const globalPrefix = configService.get<string>('GLOBAL_PREFIX');
+  if(globalPrefix){
+    app.setGlobalPrefix(globalPrefix)
+  }
+
+  //Enabling versioning
   app.enableVersioning({
     type: VersioningType.URI
   }
-)
+   )
 
-  const port = process.env.PORT || 3000;
+  const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
@@ -25,3 +35,6 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+
+
