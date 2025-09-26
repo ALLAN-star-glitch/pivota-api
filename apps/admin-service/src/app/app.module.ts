0cff2107bd14cace20d984735c71799787e8b422
Allan M <admin@EBLMICTHTGXHVWVXW.local>
2025-09-26 15:35:37 +0300

    added prisma to admin-service

--- apps/admin-service/src/app/app.module.ts
+++ apps/admin-service/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({  
+      isGlobal: true, //Make config available accross all modules
+      envFilePath: [`.env.${process.env.NODE_ENV || 'dev'}`],
+     }), // Loads .env.dev or .env.prod depending on NODE_ENV
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
